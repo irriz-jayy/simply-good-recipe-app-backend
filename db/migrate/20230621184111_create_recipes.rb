f3204283619d3c8574908439bf4444e2f4286824
jennifer sammy <jensam209@gmail.com>
2023-06-21 22:31:02 +0300

    create models,contollers,tables

---
+++ db/migrate/20230621184111_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :country_of_origin
+      t.text :description
+      t.text :ingredients
+      t.text :directions
+      t.integer :number_of_people_served
+
+      t.timestamps
+    end
+  end
+end
