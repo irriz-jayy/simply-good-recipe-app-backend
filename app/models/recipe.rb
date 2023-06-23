d66b08646dc1a2920aa73315aa41f3a95554004b
irriz-jayy <jayfelix789@gmail.com>
2023-06-23 12:22:43 +0300

    added validations to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,13 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :bookmarks
   has_many :ratings
+
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :country_of_origin, presence: true
+  validates :description, presence: true
+  validates :ingredients, presence: true
+  validates :directions, presence: true
+  validates :number_of_people_served, presence: true
+
 end
