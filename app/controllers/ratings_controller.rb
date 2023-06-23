be1571ca4c829c837cd88adbc2a43d58dbad25aa
irriz-jayy <jayfelix789@gmail.com>
2023-06-23 12:32:28 +0300

    added create for rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,8 +9,22 @@ class RatingsController < ApplicationController
         render json: rating
     end
 
+    def create
+        rating=Rating.create(rating_params)
+        if rating.valid?
+            render json: rating, status: :created
+            else
+                render json:{errors:rating.errors.full_messages}, status: :unprocessable_entity
+            end
+        end
+
+
     private
     def find_rating
         Rating.find(params[:id])
+    end
+
+    def rating_params
+        params.require(:rating).permit(:id,:user_id,:recipe_id,:rating)
     end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,8 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+
+  validates :user_id,presence: true
+  validates :recipe_id, presence: true
+  validates :rating, presence: true
 end
