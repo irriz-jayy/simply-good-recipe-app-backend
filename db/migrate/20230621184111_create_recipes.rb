0ebacf4b4de40580aa148a1a6aa6f84e7d9a302b
irriz-jayy <jayfelix789@gmail.com>
2023-06-23 12:01:20 +0300

    added minor validtioons exempting null values

--- db/migrate/20230621184111_create_recipes.rb
+++ db/migrate/20230621184111_create_recipes.rb
@@ -2,12 +2,12 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :name
-      t.string :country_of_origin
-      t.text :description
-      t.text :ingredients
-      t.text :directions
-      t.integer :number_of_people_served
+      t.string :name, null:false
+      t.string :country_of_origin , null:false
+      t.text :description, null:false
+      t.text :ingredients, null:false
+      t.text :directions, null:false
+      t.integer :number_of_people_served, null:false
 
       t.timestamps
     end
