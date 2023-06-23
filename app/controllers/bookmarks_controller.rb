2f637cdf283997caf407ebface541d700c60c7ee
irriz-jayy <jayfelix789@gmail.com>
2023-06-23 12:40:18 +0300

    created bookmark create function

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,8 +9,22 @@ class BookmarksController < ApplicationController
         render json: bookmark
     end
 
+    def create
+        bookmark = Bookmark.create(bookmark_params)
+        if bookmark.valid?
+            render json:bookmark, status: :created
+        else 
+            render json: {errors:bookmark.errors.full_messages},status: :unprocessable_entity
+        end
+    end
+
+
     private
     def find_bookmark
         Bookmark.find(params[:id])
+    end
+
+    def bookmark_params
+        params.require(:bookmark).permit(:id,:user_id,:recipe_id)
     end
 end
