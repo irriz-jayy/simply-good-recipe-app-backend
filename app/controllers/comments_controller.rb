189a5095a983ebe69f1fcaf052db811dfd174beb
irriz-jayy <jayfelix789@gmail.com>
2023-06-23 12:36:16 +0300

    added create for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,21 @@ class CommentsController < ApplicationController
         render json: comment
     end
 
+    def create
+        comment=Comment.create(comment_params)
+        if comment.valid?
+            render json: comment, status: :ok
+        else
+            render json: {errors:comment.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
     private
     def find_comment
         Comment.find(params[:id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:id,:user_id, :recipe_id,:comment)
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+
+  validates :user_id, presence: true
+  validates :recipe_id, presence: true
+  validates :comment, presence: true
 end
