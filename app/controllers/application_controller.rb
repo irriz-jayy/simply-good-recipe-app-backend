65fb268ec01d8235b685ed5a1237b81236cb1569
irriz-jayy <jayfelix789@gmail.com>
2023-06-25 17:33:06 +0300

    solved the profile issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ end
 def current_user
     if decoded_token
         user_id = decoded_token[0]['user_id']
-        @user = User.find_by(id: user.id)
+        @user = User.find_by(id: user_id)
     end
 end
 
